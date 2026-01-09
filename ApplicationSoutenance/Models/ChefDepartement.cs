b716236fc4513e7a9b5836114a17778353457e4e
Tashia04 <tatafatim30@gmail.com>
2026-01-09 00:42:16 +0000

    Implementation de la classe Chef Departement

--- ApplicationSoutenance/Models/ChefDepartement.cs
+++ ApplicationSoutenance/Models/ChefDepartement.cs
@@ -1,12 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ApplicationSoutenance.Models
 {
-    public class ChefDepartement
+    public class ChefDepartement:Utilisateur
     {
+        // FK vers Departement. Nullable si un chef peut ne pas être assigné.
+
+        public int? DepartementId { get; set; }
+        [ForeignKey("DepartementId")]
+
+        public virtual Departement Departement { get; set; }
     }
 }
